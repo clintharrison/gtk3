5f6b1aa356403bacd43d00a09bfd5deb723cef1c
Clint Harrison <243356+clintharrison@users.noreply.github.com>
2026-05-18 00:29:18 -0400

    Remove patch-toolchain.sh in favor of kindle-sdk fork

--- .devcontainer/setup-toolchain.sh
+++ .devcontainer/setup-toolchain.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 ############
 # TODO: try with kindlepw5-5.16.2.1.1.tar.gz
-KINDLE_SDK_URL="https://github.com/Sighery/kindle-sdk/releases/download/build-20260325/scribe1-5.17.3.tar.gz"
-KINDLE_SDK_SHA="8d80e019cc64826822c002712bf44e5131e8f64853a647c0cbb8a2fea82b42dd"
+KINDLE_SDK_URL="https://github.com/clintharrison/kindle-sdk/releases/download/build-20260518/scribe1-5.17.3.tar.gz"
+KINDLE_SDK_SHA="358cd3090a52d9d28d2111b99876af9a866ec4cf818b0ebee7744bbf94006a72"
 ############
 
 sdk_filename="${KINDLE_SDK_URL##*/}"
@@ -20,7 +20,6 @@ if [[ "${downloaded_sha}" != "${KINDLE_SDK_SHA}" ]]; then
 	echo "checksum mismatch: expected ${KINDLE_SDK_SHA} got ${downloaded_sha}"
 	exit 1
 fi
-
 
 set -x
 
